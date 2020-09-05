fdbb8b36ab2009c7ce3083ea045e3c753b1e3f85
Banji <khanjani1997@gmail.com>
2020-09-05 14:08:28 +0430

    added 'register()' function to the flaskblog.py and changed the hello function to 'home' function, cause the first name was incorrect

--- flaskblog.py
+++ flaskblog.py
@@ -21,12 +21,17 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
     return render_template("about.html", title = "About")
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title = "Register", form=form)
 if __name__ == '__main__':
     app.run(debug=True)
     
