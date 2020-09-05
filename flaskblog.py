a542c556d89cd168bf127e9f0a300d8b767d8eba
Banji <khanjani1997@gmail.com>
2020-09-05 18:13:14 +0430

    flash 'Account created for [user] successfully, using flash function

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -33,6 +33,8 @@ def about():
 @app.route("/register", methods=["POST", "GET"])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Accounte created for {form.username}", "success")
     return render_template("register.html", title = "Register", form=form)
 
 @app.route("/login")
