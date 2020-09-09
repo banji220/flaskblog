518307961e3cdd7eb0d88a82125f4af7546cc243
Banji <khanjani1997@gmail.com>
2020-09-09 18:08:24 +0430

    Added placeholder for username which is '@username', and a place holder text for password which is 'Password must be between 4-20 character'

--- forms.py
+++ forms.py
@@ -3,11 +3,11 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField("Username", validators = [DataRequired(), Length(min = 4, max = 20)])
+    username = StringField("Username", validators = [DataRequired(), Length(min = 4, max = 20)], render_kw={"placeholder": "@username"})
     
     email = StringField("Email", validators = [DataRequired(), Email()],render_kw={"placeholder": "email@example.com"})
     
-    password = PasswordField("Password", validators = [DataRequired()])
+    password = PasswordField("Password", validators = [DataRequired()], render_kw={"placeholder": "Password must be between 4-20 character"})
     
     confirm_password = PasswordField("Confirm Password", validators = [DataRequired(), EqualTo("password")])
     
