45449f66c8c0f0f87d30ffcd282dbf0028cc91bf
Banji <khanjani1997@gmail.com>
2020-09-12 23:08:29 +0430

    Import SQLAlchemy

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "SjG_2aPTAvf_VhRL-cWz2aObx8ylghvr"
