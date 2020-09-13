5f395c9febd23965375060b45c76b38520ad8b0a
Banji <khanjani1997@gmail.com>
2020-09-13 15:27:14 +0430

    After making 'User()' class, I created 'id', 'username', 'email', 'image_file' and 'password' columns for our database

--- flaskblog.py
+++ flaskblog.py
@@ -11,8 +11,13 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.string(120), unique=True, nullable=False)
+    image_file = db.Column(db.string(20), nullable=False, default="default.jpg")
+    password = db.Column(db.string(60), nullable=False)
     
-
+    
+    
 
 posts = [
     {
