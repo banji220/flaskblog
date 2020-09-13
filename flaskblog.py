9cb992f37df29504548a312a7e53ffaa6baf6a35
Banji <khanjani1997@gmail.com>
2020-09-13 16:04:36 +0430

    I made another database model(class) which is called Post() model for user posts

--- flaskblog.py
+++ flaskblog.py
@@ -19,6 +19,11 @@ class User(db.Model):
     
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}' )"
+    
+
+class Post(db.Model):
+
+    
 
 posts = [
     {
