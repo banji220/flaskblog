b6d8a538886bd36e246724075ccfd76fbed17006
Banji <khanjani1997@gmail.com>
2020-09-13 17:27:51 +0430

    Making a user_id column for Post table and using ForeignKey to get the user.id from User model

--- flaskblog.py
+++ flaskblog.py
@@ -27,6 +27,7 @@ class Post(db.Model):
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey("user.id") , nullable=False)
     
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')" 
