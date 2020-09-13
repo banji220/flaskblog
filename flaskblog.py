fb6f6c5cb55ec60141274ed909ee300d953c5645
Banji <khanjani1997@gmail.com>
2020-09-13 16:57:41 +0430

    I added def __repr__ which is a magic method to the post model

--- flaskblog.py
+++ flaskblog.py
@@ -17,7 +17,6 @@ class User(db.Model):
     image_file = db.Column(db.string(20), nullable=False, default="default.jpg")
     password = db.Column(db.string(60), nullable=False)
     
-    
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}' )"
     
@@ -26,6 +25,11 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')" 
+    
     
     
     
