c6f13f0393a398826725700f3a3bad5d10b733dd
Banji <khanjani1997@gmail.com>
2020-09-13 14:40:43 +0430

    Instantiated the SQLAlchemy

--- flaskblog.py
+++ flaskblog.py
@@ -4,6 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "SjG_2aPTAvf_VhRL-cWz2aObx8ylghvr"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db "
+db = SQLAlchemy(app)
+
+
+
+    
+
 
 posts = [
     {
