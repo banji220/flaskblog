0acacdf3b3bfeba6e568155667c7923f046e7e52
Banji <khanjani1997@gmail.com>
2020-09-13 17:31:46 +0430

    Change 'string' which has letter 's' with 'String' to fix the bug

--- flaskblog.py
+++ flaskblog.py
@@ -13,9 +13,9 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.string(120), unique=True, nullable=False)
-    image_file = db.Column(db.string(20), nullable=False, default="default.jpg")
-    password = db.Column(db.string(60), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
     posts = db.relationship("Post", backref="author", lazy=True)
     
     def __repr__(self):
