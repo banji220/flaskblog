e341cecd39f3501882383e32f963c0c4bd1b0e92
Banji <khanjani1997@gmail.com>
2020-09-20 23:43:45 +0330

    Removing sqlalchemy and flask and form imports from flaskblog.py and move them intor __init__.py module

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
 
-
-
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "SjG_2aPTAvf_VhRL-cWz2aObx8ylghvr"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db "
-db = SQLAlchemy(app)
  
 from models import User, Post
 
