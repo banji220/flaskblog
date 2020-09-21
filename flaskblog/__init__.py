be75b538823588dc8a4a33f2eba0c73c470ebcef
Banji <khanjani1997@gmail.com>
2020-09-21 13:17:02 +0330

    importing routes to the __init__.py after app and db to prevent circular error

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -8,3 +8,5 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "SjG_2aPTAvf_VhRL-cWz2aObx8ylghvr"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db "
 db = SQLAlchemy(app)
+
+from flaskblog import routes
