39d1e301207db48f903b3d9210be7181f8a2d8d8
Banji <khanjani1997@gmail.com>
2020-09-26 10:48:48 +0330

    Imported logout_user to the routes.py

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user,logout_user
 
 posts = [
     {
