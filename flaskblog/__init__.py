dbc1640ecec48ec864b24d4a8b62947cbcba5f26
Banji <khanjani1997@gmail.com>
2020-09-26 17:49:05 +0330

    Fixed a little bug!

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -10,6 +10,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db "
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manger.login_view = "login"
+login_manager.login_view = "login"
 from flaskblog import routes
 
