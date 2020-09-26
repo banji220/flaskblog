8f27daffae7e1f09d5d9f29f6931635190690677
Banji <khanjani1997@gmail.com>
2020-09-26 18:01:24 +0330

    Comment on login.view for better understanding

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -10,6 +10,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db "
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+#*login_view actually will work like a 'url_for()' and the line below will redirect the account page 
 login_manager.login_view = "login"
 from flaskblog import routes
 
