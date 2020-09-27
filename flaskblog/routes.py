15ad996c424900847a2e05428a0b92ec8b1308a7
Banji <khanjani1997@gmail.com>
2020-09-27 11:39:30 +0330

    Imported request object to the routes

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
