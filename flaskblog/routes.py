4fe098f5057313ef76915374b64a8ffcaac81d73
Banji <khanjani1997@gmail.com>
2020-09-28 16:50:27 +0330

    Fixed a bug

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -71,5 +71,5 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
-    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
+    image_file = url_for("static", filename="profile_pics/monopoly.png" + current_user.image_file)
     return render_template("account.html", title = "Account", image_file=image_file) 
