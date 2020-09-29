24c3691831597b70345ba838eddcd6bfdaa92ce7
Banji <khanjani1997@gmail.com>
2020-09-29 22:44:36 +0330

    Imported current_user to the forms.py

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField 
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
