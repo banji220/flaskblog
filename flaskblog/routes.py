16166f6bff5718aad251268716fd19e2fd308bca
Banji <khanjani1997@gmail.com>
2020-09-29 22:50:02 +0330

    Imported UpdateAccountForm to the routes.py

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user,logout_user, login_required
 
