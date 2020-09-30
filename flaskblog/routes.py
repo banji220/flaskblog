cb597fa387416472974f207ad3bd3d63968cba42
Banji <khanjani1997@gmail.com>
2020-09-30 10:50:15 +0330

    Fixed a bug

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -73,4 +73,4 @@ def logout():
 def account():
     form = UpdateAccountForm()
     image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
-    return render_template("account.html", title = "Account", image_file=image_file, form=form)  
+    return render_template("account.html", title = "Account", image_file=image_file, form=form)
