008827211047db63b854e4ee2d66431a05a7addc
Banji <khanjani1997@gmail.com>
2020-09-30 16:26:49 +0330

    Due to an Method not allowed err, I had to add POST method to the account route as list with GET method, so everything should work correctly

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -68,7 +68,7 @@ def logout():
     return redirect(url_for("home"))
 
 
-@app.route("/account")
+@app.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
     form = UpdateAccountForm()
