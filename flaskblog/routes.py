3d19729614c0f56f5b769bf2f36ac06d2863db4a
Banji <khanjani1997@gmail.com>
2020-10-01 14:37:09 +0330

    Fixed a bug

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -40,7 +40,7 @@ def register():
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash(f"Account has been created for with ❤️! You are now able to log in", "success")
+        flash(f"Account has been created for with ❤️! You are now be able to log in", "success")
         return redirect(url_for("login"))
     return render_template("register.html", title = "Register", form=form)
 
