31572ee3ff5b1f92ec0217aceb48c75e4851805e
Banji <khanjani1997@gmail.com>
2020-10-10 15:43:24 +0330

    Redirect the account page to the 'Home' page after updating

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -77,7 +77,7 @@ def account():
         current_user.email = form.email.data
         db.session.commit()
         flash("Your account has been updated!", "success")
-        return redirect(url_for("account"))
+        return redirect(url_for("home"))
     elif request.method == "GET":
         form.username.data = current_user.username
         form.email.data = current_user.email
