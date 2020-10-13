93e93e67845ba915f00f7fc04010c2156baae94b
Banji <khanjani1997@gmail.com>
2020-10-13 12:29:11 +0330

    Made a variable which is called picture_path to join the path of a picture file and the file name

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -72,8 +72,9 @@ def logout():
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
-    f_name, f_ext = os.path.splitext(form_picture.filename)
+    _, f_ext = os.path.splitext(form_picture.filename)
     picture_filename = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, "static/profile_pics", picture_filename)
 
 @app.route("/account", methods=["GET", "POST"])
 @login_required
