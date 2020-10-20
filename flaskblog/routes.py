a926a389fb306b3804f3e37867b57d648edde8ea
Banji <khanjani1997@gmail.com>
2020-10-20 21:48:52 +0330

    I completed the section for updating post and redirect users the specific page which is single page of their posts

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -143,8 +143,14 @@ def update_post(post_id):
     if post.author != current_user:
         abort(403)
     form = PostForm()
-    form.title.data = post.title
-    form.content.data = post.content
-    db.session.commit()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your post has been updated!", "success")
+        return redirect(url_for("post", post_id=post.id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
     return render_template("create_post.html", title="Update Post", form=form, legend="Update Post")
     
