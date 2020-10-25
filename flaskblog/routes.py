0cf72710eb9359784809d41ee66a517da749245d
Banji <khanjani1997@gmail.com>
2020-10-25 19:17:52 +0330

    Added some spaces between routes

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -172,6 +172,9 @@ def delete_post(post_id):
     flash("Your post has been deleted!", "success")
     return redirect(url_for("home"))
 
+
+
+
 @app.route("/user/<str:username>")
 def user_posts(username):
     page = request.args.get('page', 1, type=int)
