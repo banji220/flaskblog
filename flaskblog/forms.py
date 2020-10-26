8228f64e849c3d169fcd6ae3cf5714b12e31e386
Banji <khanjani1997@gmail.com>
2020-10-27 00:03:39 +0330

    I made a new class and called it RequestResetForm, so users can reset their password and recieve a email to reset it

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,9 +1,13 @@
+from logging import PlaceHolder
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+
+
+
 
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators = [DataRequired(), Length(min = 4, max = 20)], render_kw={"placeholder": "@username"})
@@ -32,11 +36,10 @@ class RegistrationForm(FlaskForm):
         email = User.query.filter_by(email=email.data).first()
         if email:
             raise ValidationError("That email is already taken!")
-        
-
-        
-        
-        
+ 
+ 
+ 
+       
 class LoginForm(FlaskForm):
     email = StringField("Email", validators = [DataRequired(), Email()])
     
@@ -45,6 +48,7 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField("Remember Me")
     
     submit = SubmitField("Login")
+    
     
     
     
@@ -77,3 +81,15 @@ class PostForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
     content = TextAreaField("Content", validators=[DataRequired()])
     submit = SubmitField("Post")
+    
+    
+    
+    
+class RequestResetForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()], render_kw={"placeholder":"mail@example.com"})
+    submit = SubmitField("Request Password Reset")
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError("There is no account with that email, You must register first!")
