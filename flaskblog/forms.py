3de8efb1998e321ea9e096b571ed69641be6584b
Banji <khanjani1997@gmail.com>
2020-10-27 00:30:33 +0330

    Made new class for PasswordResetForm, after users recieved their reset password email and then they clicked on the reset password button, they can reset their password in this Field

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -93,3 +93,10 @@ class RequestResetForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is None:
             raise ValidationError("There is no account with that email, You must register first!")
+
+
+
+class PasswordResetForm(FlaskForm):
+    password = PasswordField("Password", validators=[DataRequired()], render_kw={"placeholder": "Password must be between 4-20 character"})
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")] )
+    submit = SubmitField("Reset Password")
