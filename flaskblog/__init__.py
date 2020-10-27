ef649ff376a3bc75b910f60ed57339d4aa4a9919
Banji <khanjani1997@gmail.com>
2020-10-27 19:10:24 +0330

    Imported Mail class into the __init__.py to initialize it and configure it

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 app = Flask(__name__)
