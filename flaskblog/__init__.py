ed13083d394f5f708a304b6921bf93af37882e5f
Banji <khanjani1997@gmail.com>
2020-10-27 20:45:19 +0330

    Initialized 'Mail Server', 'Mail Port', 'Mail TLS', 'Mail Username' and 'Mail Password'

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -14,6 +15,13 @@ login_manager = LoginManager(app)
 #*login_view actually will work like a 'url_for()' and the line below will redirect the account page 
 login_manager.login_view = "login"
 login_manager.login_message_category = "warning"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
+app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASS")
+mail = Mail(app)
 
 from flaskblog import routes
+
 
