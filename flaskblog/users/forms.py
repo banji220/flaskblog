8f80b00393c569e9ce4905254727e614866d2c30
Banji <khanjani1997@gmail.com>
2020-10-30 19:27:23 +0330

    Imported all of the packages and modules to all of the files and fixed errors, and removed old forms.py and routes.py

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,3 +1,9 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators = [DataRequired(), Length(min = 4, max = 20)], render_kw={"placeholder": "@username"})
