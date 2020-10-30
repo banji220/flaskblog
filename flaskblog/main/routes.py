4ff0ccd38826eae05ef65851a364721f21bd5eba
Banji <khanjani1997@gmail.com>
2020-10-30 17:28:51 +0330

    Moved routes that are related to the main

---
+++ flaskblog/main/routes.py
@@ -0,0 +1,20 @@
+from flask import Blueprint
+
+main = Blueprint("main", __name__)
+
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    page = request.args.get("page", 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=2)
+    return render_template("home.html", posts=posts)
+
+
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title = "About")
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -10,19 +10,6 @@ from flaskblog.models import User, Post
 from flask_login import login_user, current_user,logout_user, login_required
 from flask_mail import Message
 
-@app.route("/")
-@app.route("/home")
-def home():
-    page = request.args.get("page", 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=2)
-    return render_template("home.html", posts=posts)
-
-
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html", title = "About")
 
 
 
