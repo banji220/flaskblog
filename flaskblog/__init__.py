19f5788569bc37c62c243103686a349156cf9a9b
Banji <khanjani1997@gmail.com>
2020-11-01 10:56:17 +0330

    Added new file config.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -7,19 +7,14 @@ from flask_mail import Mail
 
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "SjG_2aPTAvf_VhRL-cWz2aObx8ylghvr"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db "
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 #*login_view actually will work like a 'url_for()' and the line below will redirect the account page 
 login_manager.login_view = "users.login"
 login_manager.login_message_category = "warning"
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
-app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASS")
+
 mail = Mail(app)
 
 
