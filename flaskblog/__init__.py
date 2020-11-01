3db505b095a731aefcd038e66e03d7964e0d6470
Banji <khanjani1997@gmail.com>
2020-11-01 15:24:06 +0330

    Fixed a bug: from flablog import Config ----> from flaskblog.config import Config

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from flaskblog import Config
+from flaskblog.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
