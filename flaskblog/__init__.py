71888c7db1c43d7e0f6819f9b33498c8eb26bdfa
Banji <khanjani1997@gmail.com>
2020-11-02 13:09:37 +0330

    Fixed bugs: 1. in the run.py: from flask import create_app ----> from flasblog import create_app

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -9,27 +9,20 @@ from flaskblog.config import Config
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
-#*login_view actually will work like a 'url_for()' and the line below will redirect the account page 
-login_manager.login_view = "users.login"
-login_manager.login_message_category = "warning"
-
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
 mail = Mail()
-
-
-
-
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
-    
+
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-    
-    
+
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
@@ -38,4 +31,5 @@ def create_app(config_class=Config):
     app.register_blueprint(posts)
     app.register_blueprint(main)
     app.register_blueprint(errors)
+
     return app
